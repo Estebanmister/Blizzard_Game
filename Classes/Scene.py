88ad39a4ee6d083eaed8eab68ce805a2d5ac8d43
Esteban <36859022+Estebanmister@users.noreply.github.com>
2022-06-06 13:47:14 -0400

    Added ID and fixed docstring on scene class

--- Classes/Scene.py
+++ Classes/Scene.py
@@ -1,14 +1,15 @@
 class Scene:
-    def __init__(self, length, width, background_image, linked_rooms=None, music=None):
+    def __init__(self, ID, length, width, background_image, linked_rooms = None, music=None):
         """
         Create a Scene object. Add entities to the list Scene.entities
 
         :param length: (integer) Length of the room in tiles
         :param width: (integer) Width of the room in tiles
-        :param background_image: (Image?) Image data for the background
+        :param background_image: (pygame.Surface) Image data for the background
         :param linked_rooms: (Dictionary, {'up':None, 'down':None, 'left':None, 'right':None}) Other Scene entities that the room links to from each side
         :param music: (Sound) Sound data to play once the scene becomes active, defaults to None
         """
+        self.ID = ID
         self.background_image = background_image
         self.music = music
         # Dictionaries are mutable, so they shouldn't be used as default parameters
