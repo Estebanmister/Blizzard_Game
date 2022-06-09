2524d1380d7feb69b2fdec32f743511fc818bd56
Esteban <36859022+Estebanmister@users.noreply.github.com>
2022-06-09 13:58:13 -0400

    fixed an error on the wall class

--- Classes/wall.py
+++ Classes/wall.py
@@ -4,8 +4,8 @@ from Classes.entity import Entity
 class Wall(Entity):
 
     # Wall inherits Entity and contain a tuple of coordinates that is just... illegal
-    def __init__(self, coord, sprite, index):
-        Entity.__init__(self, coord, sprite, index)
+    def __init__(self, coord, sprite, index=0, args=()):
+        Entity.__init__(self, coord, sprite, index, args=args)
         self.__coord = coord
 
     def get_coord(self):
