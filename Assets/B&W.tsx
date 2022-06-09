a972c76d6b7fce83544dfad621f0f34f71bc4c6a
Sam <106622567+Silver-Robin@users.noreply.github.com>
2022-06-09 00:30:00 -0400

    whole lotta assets

---
+++ Assets/B&W.tsx
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="B&amp;W" tilewidth="16" tileheight="16" tilecount="20" columns="0" backgroundcolor="#000000">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="56" type="Wall">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile56.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5" y="1" width="6" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="57" type="Wall">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile57.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="5" width="14" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="2" type="Wall">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile2.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5" y="1" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="18" type="Wall">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile18.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="0" type="Wall">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile0.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5" y="5" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="16" type="Wall">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile16.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="5" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="1" type="Wall">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile1.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5" y="1" width="10" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="17" type="Wall">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile17.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="10" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="8" type="Wall">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile8.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="5" width="14" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="10" type="Wall">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile10.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="14" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="9" type="Wall">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile9.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="58" type="Door">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile58.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7" y="1" width="2" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="64" type="Door">
+  <image width="16" height="16" source="../../../../../../Downloads/Untitled445_20220602115329.png"/>
+ </tile>
+ <tile id="59" type="Door">
+  <image width="16" height="16" source="../../../../../../Downloads/Ascii-wall-like tileset by GibberishDev/tiles/tile59.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="7" width="14" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="63" type="Door">
+  <image width="16" height="16" source="../../../../../../Downloads/Untitled445_20220602115326.png"/>
+ </tile>
+ <tile id="65" type="Note">
+  <image width="16" height="16" source="../../../../../../Downloads/Untitled445_20220602121002.png"/>
+ </tile>
+ <tile id="66" type="Health">
+  <image width="16" height="16" source="../../../../../../Downloads/Untitled445_20220602115703.png"/>
+ </tile>
+ <tile id="67" type="Vent">
+  <image width="16" height="16" source="../../../../../../Downloads/Untitled445_20220602115921.png"/>
+ </tile>
+ <tile id="69">
+  <image width="16" height="16" source="../../../../../../Downloads/Untitled445_20220602181758.png"/>
+ </tile>
+ <tile id="70">
+  <image width="16" height="16" source="../../../../../../Downloads/Untitled445_20220602181747.png"/>
+ </tile>
+</tileset>
