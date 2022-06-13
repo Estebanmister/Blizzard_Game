150d470560065f2da0258bd464f08026e514a378
UtkarshP20 <106771626+UtkarshP20@users.noreply.github.com>
2022-06-13 13:44:05 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -4,7 +4,9 @@ from data_loader import *
 from Classes import visuals
 import os
 
-
+#define some variables, what FPS game will run at
+#a basic white tuple to make writing colors easier
+#set desired width and height game will run at later
 FPS = 60
 white = (255,255,255)
 width, height = 700,700
@@ -13,20 +15,44 @@ width, height = 700,700
 scY = 0
 scX = 0
 
+#imports from other files, scene, player object
+currentDungeon = load_dungeon('World/Overworld')
+currentScene = currentDungeon.head
+player_obj = currentScene.get_entity('Player0') 
 
+#set our screen size
 screen = pygame.display.set_mode((width,height))
 
+#load a placeholder sprite
 placeHolderSprite =  pygame.image.load('Assets/Sprites/placeholder.png')
 placeHolderSprite = pygame.transform.scale(placeHolderSprite,(70,70))
+
+
+def player_movement(keys_pressed):
+    if keys_pressed[pygame.K_a]:
+        print("works")
+        player_obj.move('left')
+    if keys_pressed[pygame.K_d]:
+        player_obj.move('right')
+        print("works")
+    if keys_pressed[pygame.K_w]:
+        player_obj.move('up')
+        print("works")
+    if keys_pressed[pygame.K_s]:
+        player_obj.move('down')
+        print("works")
+
 
 def Main():
     clock = pygame.time.Clock()
     run = True
-
-    currentDungeon = load_dungeon('World/Overworld')
-    currentScene = currentDungeon.head
-
+    
+    global keys_pressed
+    global currentDungeon
+    global currentScene
+    global player_obj
     global scY, scX
+
     scX = width/currentScene.width
     scY = height/currentScene.length
     
@@ -37,8 +63,9 @@ def Main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            keys_pressed = pygame.key.get_pressed()
+        player_movement(keys_pressed)
         draw_display(currentScene)
-        currentScene.update_all()
             
 
 def draw_display(scene):
@@ -46,7 +73,6 @@ def draw_display(scene):
     for entity in scene.get_all_entities():
         coordinateDraw = entity.coord 
         screen.blit(pygame.transform.scale(entity.sprite,(scX,scY)),((coordinateDraw[0]* scX,coordinateDraw[1]*scY)))
-    
     pygame.display.update()
 
 Main()
