6642c8d22f371f2173efbd4079d64fa406697447
Esteban <36859022+Estebanmister@users.noreply.github.com>
2022-06-13 13:48:35 -0400

    Revert "Format"
    This reverts commit c190a40f6cb70c66e379dfdd58bc4ceaccbc4989.

--- main2.py
+++ main2.py
@@ -4,19 +4,20 @@ from data_loader import *
 from Classes import visuals
 import os
 
-FPS = 60
-white = (255, 255, 255)
-width, height = 700, 700
 
-# calculate the sprite scale using screen -- calculation is done in Main()
+FPS = 60
+white = (255,255,255)
+width, height = 700,700
+
+#calculate the sprite scale using screen -- calculation is done in Main()
 scY = 0
 scX = 0
 
-screen = pygame.display.set_mode((width, height))
 
-placeHolderSprite = pygame.image.load('Assets/Sprites/placeholder.png')
-placeHolderSprite = pygame.transform.scale(placeHolderSprite, (70, 70))
+screen = pygame.display.set_mode((width,height))
 
+placeHolderSprite =  pygame.image.load('Assets/Sprites/placeholder.png')
+placeHolderSprite = pygame.transform.scale(placeHolderSprite,(70,70))
 
 def Main():
     clock = pygame.time.Clock()
@@ -26,8 +27,9 @@ def Main():
     currentScene = currentDungeon.head
 
     global scY, scX
-    scX = width / currentScene.width
-    scY = height / currentScene.length
+    scX = width/currentScene.width
+    scY = height/currentScene.length
+    
 
     pygame.display.set_caption("Blizzard")
     while run:
@@ -37,16 +39,14 @@ def Main():
                 run = False
         draw_display(currentScene)
         currentScene.update_all()
-
+            
 
 def draw_display(scene):
-    screen.blit(pygame.transform.scale(scene.background_image, (width, height)), (0, 0))
+    screen.blit(pygame.transform.scale(scene.background_image,(width,height)),(0,0)) 
     for entity in scene.get_all_entities():
-        coordinateDraw = entity.coord
-        screen.blit(pygame.transform.scale(entity.sprite, (scX, scY)),
-                    ((coordinateDraw[0] * scX, coordinateDraw[1] * scY)))
-
+        coordinateDraw = entity.coord 
+        screen.blit(pygame.transform.scale(entity.sprite,(scX,scY)),((coordinateDraw[0]* scX,coordinateDraw[1]*scY)))
+    
     pygame.display.update()
-
 
 Main()
