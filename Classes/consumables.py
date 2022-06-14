68a190b9d5d53e18f750c23feb6e999e28fd7b3f
Ray <richard040417@gmail.com>
2022-06-14 17:51:17 +0800

    Add doc to Consumables, test

--- Classes/consumables.py
+++ Classes/consumables.py
@@ -4,22 +4,45 @@ from random import randint
 
 
 class WaterBottle(Entity):
+    # class initializer
     def __init__(self, coord, sprite, player_stats, index=0, args=()):
+        """
+        WaterBottle entity, inherits Entity.
+
+        :param player_stats: The playerStats object
+        :param index: (Integer) How many of these same entities were already created
+        :param coord: (List of Integers) X and Y coordinates in tiles
+        :param sprite: (Surface) Pygame image to display this entity
+        :param args: (List of any) Not used in base entity, but may have different functionalities on different subclasses
+        """
         Entity.__init__(self, coord, sprite, index, args)
         self.__player_stats = player_stats
 
     def drink(self):
+        """
+        Adds a random hydration to the player (between 3-6)
+        """
         self.__player_stats.add_thirst(randint(3, 6))
 
 
 class CannedFood(Entity):
+    # class initializer
     def __init__(self, coord, sprite, player_stats, index=0, args=()):
+        """
+        CannedFood entity, inherits Entity.
+
+        :param player_stats: The playerStats object
+        :param index: (Integer) How many of these same entities were already created
+        :param coord: (List of Integers) X and Y coordinates in tiles
+        :param sprite: (Surface) Pygame image to display this entity
+        :param args: (List of any) Not used in base entity, but may have different functionalities on different subclasses
+        """
+
         Entity.__init__(self, coord, sprite, index, args)
         self.__player_stats = player_stats
 
     def eat(self):
+        """
+        Adds a random saturation to the player (between 3-6)
+        """
         self.__player_stats.add_hunger(randint(3, 6))
-
-
-test = PlayerStats()
-food = CannedFood()
