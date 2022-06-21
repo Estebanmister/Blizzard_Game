e7d46b7d4c63dc49775e7445199a7470c2350ddb
Ray <richard040417@gmail.com>
2022-06-22 01:01:56 +0800

    Update collisionentity.py

--- Classes/collisionentity.py
+++ Classes/collisionentity.py
@@ -9,8 +9,8 @@ class CollisionEntity(Entity):
         Creates a collision entity, that will add an illegal coordinate to the scene, to restrict movement
 
         :param coord: (tuple) X and Y coordinates in tiles, in form of (x,y)
-        :param sprite: (Surface) Pygame image to display for a wall entity
-        :param index: (Integer) How many wall entities were already created
+        :param sprite: (Surface) Pygame image to display for a CollisionEntity entity
+        :param index: (Integer) How many CollisionEntity entities were already created
         :param args: (List of any) Not used in base entity, but may have different functionalities on different subclasses
         """
         Entity.__init__(self, coord, sprite, index, args=args)
@@ -18,6 +18,6 @@ class CollisionEntity(Entity):
 
     def get_coord(self):
         """
-        :return: Coordinate of the wall entity
+        :return: Coordinate of the CollisionEntity entity
         """
         return self.__coord
